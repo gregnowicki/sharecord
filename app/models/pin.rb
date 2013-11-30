3051bafa0be24e8d2c4805b89fad5cf9814cffb2
Greg Nowicki <nowicki.greg@gmail.com>
2013-11-29 22:36:56 -0500

    Add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,9 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	validates :description, :length => { :minimum => 40 }
+	validates :description, presence: true, :length => { :minimum => 40 }
+  validates :image, presence: true
+  validates :description, presence: true
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
